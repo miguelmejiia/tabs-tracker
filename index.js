2cee19b5d6292a65bc156f79b4ff1f5166ca7a20
Miguel Mejia <miguelmejia92@outlook.com>
2022-02-10 18:34:18 -0600

    use of 'Template strings' for render the embdded HTML code

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const listEl = document.getElementById("list-el")
 
-inputBtn.addEventListener("click", function() {
+inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    inputEl.value = ""
+    renderLeads()
 })
+
+function renderLeads() {
+    let listItems = "";
+    for (let i = 0; i < myLeads.length; i++) {        
+        listItems += `
+        <li>
+            <a href='${myLeads[i]}' target=_blank>'${myLeads[i]}'</a>
+        </li>`
+    }
+    listEl.innerHTML = listItems
+}
